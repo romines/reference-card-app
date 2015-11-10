fae15cb352237fb55d393599443c231bd6b0fd64
Adam Romines <adam.romines@gmail.com>
2015-11-09 17:47:18 -0700

    Form submit places object on scope

--- main/mainCtrl.js
+++ main/mainCtrl.js
@@ -1,6 +1,23 @@
 angular.module('cardApp')
   .controller('mainCtrl', ['$scope', 'cardService', function ($scope, cardService) {
-    $scope.card = 'This is a sweet card';
+    // $scope.card = 'This is a sweet card';
     $scope.aboutText = 'Some stuff about the app';
     $scope.datas = cardService.getCards();
+
+    $scope.cards = [];
+
+    $scope.save = function(card) {
+      var formDefaults = {
+        title: '',
+        content: '',
+        tags: ''
+      };
+
+
+      // console.log('Save fired. Contents of card: ', card)
+      var newCard = angular.copy(card);
+      $scope.cards.push(newCard);
+      $scope.card = formDefaults;
+      $scope.cardForm.$setPristine();
+    };
 }]);
