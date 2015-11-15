a0b53510caad3606e24fcc0ac7f193896f59e9dc
Adam Romines <adam.romines@gmail.com>
2015-11-14 21:09:39 -0700

    adds delete to card edit. uses ui-sref for edit link

--- cards/cardService.js
+++ cards/cardService.js
@@ -2,16 +2,16 @@ angular.module('cardApp')
   .service('cardService', ['$firebaseArray', function ($firebaseArray) {
     var cardsUrl = 'https://reference-cards-app.firebaseio.com/cards/';
 
+
     this.getCardsRef = function () {
 
       var cardsRef = new Firebase(cardsUrl);
-
       return cardsRef;
 
-    }
+    };
 
     this.getCard = function (cardId) {
       return new Firebase(cardsUrl + cardId);
-    }
+    };
 
   }])
--- cards/edit/editCardCtrl.js
+++ cards/edit/editCardCtrl.js
@@ -4,8 +4,20 @@ angular.module('cardApp')
     $scope.card = $firebaseObject(editCardRef);
 
     $scope.saveEdits = function(){
+      // console.log($scope.card.parent());
+      $scope.card.tags = listifyTags($scope.card.tags);
+
       $scope.card.$save();
       $state.go('cards');
+    };
+
+    $scope.delete = function () {
+      $scope.card.$remove();
+      $state.go('cards');
+    }
+
+    function listifyTags(str) {
+      return str.split(/[ ,]+/);
     }
 
 }]);
